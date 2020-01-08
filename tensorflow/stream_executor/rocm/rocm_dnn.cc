6cdcc83d535d02fbd56a7fe7186dd815173a9987
Deven Desai <deven.desai.amd@gmail.com>
2020-01-08 17:46:26 +0000

    PR 783 - [DO NOT MERGE] Porting ROCm blfoat16 support to master-rocm-enhanced

--- tensorflow/stream_executor/rocm/rocm_dnn.cc
+++ tensorflow/stream_executor/rocm/rocm_dnn.cc
@@ -1652,6 +1652,8 @@ miopenDataType_t ToMIOpenDataType(
   switch (data_type) {
     case dnn::DataType::kFloat:
       return miopenFloat;
+    case dnn::DataType::kBFloat16:
+      return miopenBFloat16;
     case dnn::DataType::kHalf:
       return miopenHalf;
     case dnn::DataType::kDouble:
@@ -1710,6 +1712,8 @@ int MIOpenDataTypeToByteSize(miopenDataType_t data_type) {
       return sizeof(float);
     case miopenHalf:
       return sizeof(Eigen::half);
+    case dnn::DataType::kBFloat16:
+      return sizeof(tensorflow::bfloat16);
     default:
       LOG(FATAL) << "Invalid DNN data type: " << static_cast<int>(data_type);
   }
@@ -1726,6 +1730,7 @@ dnn::DataType GetConvAccumulatorType(dnn::DataType data_type) {
     case dnn::DataType::kDouble:
       return data_type;
     case dnn::DataType::kHalf:
+    case dnn::DataType::kBFloat16:
       // FIXME: Check if MIOpen can switch dynamically change accumulator type
       return dnn::DataType::kFloat;
     case dnn::DataType::kInt8:
